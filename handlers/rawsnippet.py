e764830457b84e11af4ee59d1371b282e4845573
Roman Podolyaka <roman.podolyaka@gmail.com>
2011-06-13 09:33:05 +0300

    Change raw snippet url.

--- handlers/rawsnippet.py
+++ handlers/rawsnippet.py
@@ -11,21 +11,22 @@ class RawSnippet(webapp.RequestHandler):
 
         Processes GET and POST requests.
 
-        Params:
-            id --- the unique identifier of snippet entry
+        Snippet id is specified as url path (part between the host name and params), i.e.:
+            GET xsnippet.tk/1/raw will return text content of snippet with id 1
     '''
 
     def get(self):
         self.post()
 
     def post(self):
-        snippet = Snippet.get_by_id(int(self.request.get('id')))
+        snippetid = int(self.request.path.split('/')[1])
+        snippet = Snippet.get_by_id(snippetid)
 
         if snippet is not None:
             self.response.headers['Content-Type'] = 'text/plain'
             self.response.out.write(snippet.content)
         else:
-            template_values = {'error': 'Snippet with id %s not found' % self.request.get('id')}
+            template_values = {'error': 'Snippet with id %s not found' % snippetid}
             path = os.path.join(os.getcwd(), 'templates', '404.html')
 
             self.error(404)
