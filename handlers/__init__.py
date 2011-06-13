aeebe57e289e43e3dd3bac070c6eb477b4a259d0
Roman Podolyaka <roman.podolyaka@gmail.com>
2011-06-13 13:31:44 +0300

    Add DownloadSnippet handler.

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,8 +2,10 @@ from mainpage import MainPage
 from newsnippet import NewSnippet
 from showsnippet import ShowSnippet
 from rawsnippet import RawSnippet
+from downloadsnippet import DownloadSnippet
 
 __all__ = ('MainPage',
            'NewSnippet',
            'ShowSnippet',
-           'RawSnippet')
+           'RawSnippet',
+           'DownloadSnippet')
---
+++ handlers/downloadsnippet.py
@@ -0,0 +1,40 @@
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+from model import Snippet
+
+class DownloadSnippet(webapp.RequestHandler):
+    '''
+        Download snippet content as a file
+
+        Processes GET and POST requests.
+
+        Snippet id is specified as url path (part between the host name and params), i.e.:
+            GET xsnippet.tk/1/download will return content of snippet with id 1 as a file
+    '''
+
+    def get(self):
+        self.post()
+
+    def post(self):
+        snippetid = int(self.request.path.split('/')[1])
+        snippet = Snippet.get_by_id(snippetid)
+
+        if snippet is not None:
+            filename = str(snippetid)
+            extension = snippet.extensions[snippet.language] if snippet.language in snippet.extensions else '.txt'
+            attachment = 'attachment; filename="%s%s"' % (filename, extension)
+
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.headers['Content-Disposition'] = attachment
+            self.response.out.write(snippet.content)
+        else:
+            template_values = {'error': 'Snippet with id %s not found' % snippetid}
+            path = os.path.join(os.getcwd(), 'templates', '404.html')
+
+            self.error(404)
+            self.response.headers['Content-Type'] = 'text/html'
+            self.response.out.write(template.render(path, template_values))
+
