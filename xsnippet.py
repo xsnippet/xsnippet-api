0eec581134c7bd781bf92f8e98327be8d3dd32da
Igor Kalnitsky <igor@kalnitsky.org>
2011-12-28 14:17:54 +0200

    Use lazy handlers.

--- xsnippet.py
+++ xsnippet.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import sys
 import os
 
@@ -6,31 +7,19 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 
 import webapp2
 
-from handlers import MainPage
-from handlers import AboutPage
-from handlers import UploadPage
-from handlers import NewSnippet
-from handlers import ShowSnippet
-from handlers import RawSnippet
-from handlers import DownloadSnippet
-from handlers import EmbedSnippet
-from handlers import ListSnippet
-from handlers import SearchSnippet
-from handlers import RecentSnippet
-from handlers import Sitemap
-
-application = webapp2.WSGIApplication([(r'/', MainPage),
-                                       (r'/about', AboutPage),
-                                       (r'/upload', UploadPage),
-                                       (r'/new', NewSnippet),
-                                       (r'/search', SearchSnippet),
-                                       (r'/([0-9]+)', ShowSnippet),
-                                       (r'/([0-9]+)/raw', RawSnippet),
-                                       (r'/([0-9]+)/download', DownloadSnippet),
-                                       (r'/([0-9]+)/embed', EmbedSnippet),
-                                       (r'/recent/?', RecentSnippet),
-                                       (r'/recent/?(\d+)?', RecentSnippet),
-                                       (r'/(\w+)/([a-zA-Z0-9_%-]+)/?', ListSnippet),
-                                       (r'/(\w+)/([a-zA-Z0-9_%-]+)/(\d+)?', ListSnippet),
-                                       (r'/sitemap.xml', Sitemap)],
-                                       debug=True)
+application = webapp2.WSGIApplication([
+    (r'/', 'handlers.MainPage'),
+    (r'/about', 'handlers.AboutPage'),
+    (r'/upload', 'handlers.UploadPage'),
+    (r'/new', 'handlers.NewSnippet'),
+    (r'/search', 'handlers.SearchSnippet'),
+    (r'/([0-9]+)', 'handlers.ShowSnippet'),
+    (r'/([0-9]+)/raw', 'handlers.RawSnippet'),
+    (r'/([0-9]+)/download', 'handlers.DownloadSnippet'),
+    (r'/([0-9]+)/embed', 'handlers.EmbedSnippet'),
+    (r'/recent/?', 'handlers.RecentSnippet'),
+    (r'/recent/?(\d+)?', 'handlers.RecentSnippet'),
+    (r'/(\w+)/([a-zA-Z0-9_%-]+)/?', 'handlers.ListSnippet'),
+    (r'/(\w+)/([a-zA-Z0-9_%-]+)/(\d+)?', 'handlers.ListSnippet'),
+    (r'/sitemap.xml', 'handlers.Sitemap')],
+    debug=True)
