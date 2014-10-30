3a2c2fb5f3268bad54e36538f75f00cb8bef2606
Roman Podoliaka <roman.podoliaka@gmail.com>
2014-10-30 18:05:17 +0200

    Make it clear default settings are for debug/tests

--- xsnippet/settings.py
+++ xsnippet/settings.py
@@ -1,3 +1,5 @@
+# these are 'debug' settings which are used for unit and functional tests
+
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite://'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 SQLALCHEMY_ECHO = True
