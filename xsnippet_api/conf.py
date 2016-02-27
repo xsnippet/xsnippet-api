735987038a4d6b73398789f1f8d1c7ed92e96951
Igor Kalnitsky <igor@kalnitsky.org>
2016-02-27 23:43:24 +0200

    Move get_conf to separate module
    It much more convenient to move `get_conf` function into separate
    module, in order to make it importable from tests, for example.

---
+++ xsnippet_api/conf.py
@@ -0,0 +1,38 @@
+"""
+    xsnippet_api.conf
+    -----------------
+
+    The module provides a function that gathers application settings
+    from various sources, combines them and returns them at once.
+
+    :copyright: (c) 2016 The XSnippet Team, see AUTHORS for details
+    :license: MIT, see LICENSE for details
+"""
+
+import os
+import configparser
+
+
+def get_conf(paths, envvar=None):
+    """Return one settings instance combines from different sources.
+
+    The idea that lies behind that function is to gather config settings
+    from different sources, including dynamic once pointed by passed
+    environment variable (usually production overrides).
+
+    :param paths: a list of paths to configuration files
+    :type paths: [str]
+
+    :param envvar: an environment variable that points to additional config
+    :type envvar: str
+
+    :return: a configuration instance
+    :rtype: :class:`configparser.ConfigParser`
+    """
+    conf = configparser.ConfigParser()
+    conf.read(paths)
+
+    if envvar in os.environ:
+        conf.read(os.environ[envvar])
+
+    return conf
